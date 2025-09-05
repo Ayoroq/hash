38b72e9e063b65153b4da1b41299e8f1fecc3a5c
Ayo <Ayoroq@gmail.com>
2025-09-04 23:40:12 -0400

    Created the Hash class, the hash function, the get and the set methods

---
+++ hash.js
@@ -0,0 +1,48 @@
+class HashMap {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.buckets = new Array(capacity);
+    this.length = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode %= this.capacity;
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      this.buckets[index] = [];
+    }
+    for (let i = 0; i < this.buckets[index].length; i++){
+      if (this.buckets[index][i].key === key){
+        this.buckets[index][i].value = value;
+        return;
+      }
+    }
+    this.buckets[index].push({ key, value });
+    this.length++;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    if (this.buckets[index]){
+      for (let i = 0; i < this.buckets[index].length; i++) {
+        if (this.buckets[index][i].key === key) {
+          return this.buckets[index][i].value;
+        }
+      }
+    }
+    return null;
+  }
+}
